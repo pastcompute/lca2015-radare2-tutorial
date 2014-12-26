6eaa73c2529a334b24acb26efaf6aa9ad20bd10c
Andrew McDonnell <bugs@andrewmcdonnell.net>
2014-12-26 12:33:06 +1030

    Update file location

--- examples/radiff_random_example.sh
+++ examples/radiff_random_example.sh
@@ -3,19 +3,22 @@
 # Create two random binary files almost the same
 #
 
+F1=temp/file1.bin
+F2=temp/file2.bin
+
 if [ -z "$JUST_DIFF" ] ; then
-dd if=/dev/urandom of=file1.bin bs=1024 count=1
-cp file1.bin file2.bin
+dd if=/dev/urandom of=$F1 bs=1024 count=1
+cp $F1 $F2
 fi
 
-$PREFIX radiff2 file1.bin file2.bin
+$PREFIX radiff2 $F1 $F2
 
-[ -z "$JUST_DIFF" ] && echo Changing 4 bytes && r2 -qnwc 's 0x12; wx 0xdeadbeef' file2.bin
-$PREFIX radiff2 file1.bin file2.bin
+[ -z "$JUST_DIFF" ] && echo Changing 4 bytes && r2 -qnwc 's 0x12; wx 0xdeadbeef' $F2
+$PREFIX radiff2 $F1 $F2
 
-[ -z "$JUST_DIFF" ] && echo Changing 4 more bytes && r2 -qnwc 's 0x212; wx 0xde2dbeef' file2.bin
-$PREFIX radiff2 file1.bin file2.bin
+[ -z "$JUST_DIFF" ] && echo Changing 4 more bytes && r2 -qnwc 's 0x212; wx 0xde2dbeef' $F2
+$PREFIX radiff2 $F1 $F2
 
-[ -z "$JUST_DIFF" ] && echo Changing 4 more bytes && r2 -qnwc 's 0x337; wx 0xdeadb3ef' file2.bin
-$PREFIX radiff2 file1.bin file2.bin
+[ -z "$JUST_DIFF" ] && echo Changing 4 more bytes && r2 -qnwc 's 0x337; wx 0xdeadb3ef' $F2
+$PREFIX radiff2 $F1 $F2
 
